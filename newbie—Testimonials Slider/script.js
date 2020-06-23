9a4561a887de9298956a16987d115fc38befbc93
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2020-06-22 21:11:42 -0700

    working on Testimonials

---
+++ newbie—Testimonials Slider/script.js
@@ -0,0 +1,30 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let index = 0;
+display(index);
+function display(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide() {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
