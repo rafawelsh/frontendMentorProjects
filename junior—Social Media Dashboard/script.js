13000528f4346716875cfcb5b8575f3f33e6e709
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2020-06-25 20:42:41 -0700

    social media dashboard added

---
+++ junior—Social Media Dashboard/script.js
@@ -0,0 +1,12 @@
+const chk = document.getElementById('chk')
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+chk.addEventListener('change', switchTheme, false);
