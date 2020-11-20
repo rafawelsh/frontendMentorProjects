ead6049b6a2bad310053b49dae88ffc10dd5f206
rafawelsh <mrrafael1912@gmail.com>
2020-11-19 22:41:46 -0800

    restarting the project to work on React hooks

---
+++ advanced—rps/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppStyled = styled.div``;
+
+export default function App() {
+	return (
+		<AppStyled>
+			<div className='app-content'></div>
+		</AppStyled>
+	);
+}
