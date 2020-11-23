79b4aa40b7a579b0342d167779d3545756e83108
rafawelsh <mrrafael1912@gmail.com>
2020-11-23 14:10:54 -0800

    Updates

---
+++ precache-manifest.bffd3dc09d5ca3e2a91cd499e53989cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73057a466d6cb020614a9dcb3d7e2ee",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/index.html"
+  },
+  {
+    "revision": "6b07665f3279dcd548a1",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/static/css/main.ece3cd5f.chunk.css"
+  },
+  {
+    "revision": "6969237528f274c03680",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/static/js/2.a5963419.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/static/js/2.a5963419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b07665f3279dcd548a1",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/static/js/main.1a1f6abc.chunk.js"
+  },
+  {
+    "revision": "3937d378ead2801788c2",
+    "url": "/frontendMentorProjects/advanced%E2%80%94rps/static/js/runtime-main.83e332d4.js"
+  }
+]);
